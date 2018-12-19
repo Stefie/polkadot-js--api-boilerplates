aa32a49eaf022e911425555324f8ae16059a1ceb
Stefanie Doll <stefie.doll@googlemail.com>
2018-12-19 15:49:55 +0100

    WIP rx version

--- demo-vanilla-js/src/api-calls/with-api-rx.js
+++ demo-vanilla-js/src/api-calls/with-api-rx.js
@@ -9,6 +9,19 @@ const createElement = (content) => {
   app.appendChild(p);
 };
 
+export const simpleConnect = async (provider) => {
+  // Retrieve the chain & node information information via rpc calls
+  // const api = await ApiRx.create(provider);
+  // const [chain, nodeName, nodeVersion, properties] = await Promise.all([
+  //   api.rpc.system.chain(),
+  //   api.rpc.system.name(),
+  //   api.rpc.system.version(),
+  //   api.rpc.system.properties()
+  // ]);
+  // createElement(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+  // console.log(properties);
+};
+
 export const listenToBlocks = async (provider) => {
   const api = await ApiRx.create(provider).toPromise();
   const subscriptionId = api.rpc.chain.subscribeNewHead().subscribe((header) => {
