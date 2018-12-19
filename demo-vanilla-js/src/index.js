6595ab7bae8ff66e245f5a3959e7f53c4e98681d
Stefanie Doll <stefie.doll@googlemail.com>
2018-12-19 15:56:13 +0100

    Even more stripped down version to start with

--- demo-vanilla-js/src/index.js
+++ demo-vanilla-js/src/index.js
@@ -1,14 +1,38 @@
 import { WsProvider } from '@polkadot/rpc-provider';
-import { simpleConnect, listenToBlocks } from './api-calls/with-api-promise';
-// import { simpleConnect, listenToBlocks } from './api-calls/with-api-rx';
+import { ApiPromise } from '@polkadot/api';
 import './style.css';
 
-// Initialise the provider to connect to the local node
-// const provider = new WsProvider('ws://127.0.0.1:9944');
+const app = document.querySelector('#content');
 const provider = new WsProvider('wss://substrate-rpc.parity.io/');
 
+const createElement = (content) => {
+  console.log(content);
+  const p = document.createElement('p');
+  p.innerHTML = content;
+  app.appendChild(p);
+};
+
+const simpleConnect = async (ws) => {
+  // Retrieve the chain & node information information via rpc calls
+  const api = await ApiPromise.create(provider);
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
+  ]);
+  createElement(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+};
+
+const listenToBlocks = async (ws) => {
+  const api = await ApiPromise.create(provider);
+  const subscriptionId = await api.rpc.chain.subscribeNewHead((header) => {
+    createElement(`Chain is at: #${header.blockNumber}`);
+  });
+  createElement(`subsciptionId: ${subscriptionId}`);
+};
+
 async function main () {
-  // simpleConnect(provider);
+  simpleConnect(provider);
   listenToBlocks(provider);
 }
 
