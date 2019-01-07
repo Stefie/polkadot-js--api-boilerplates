b3393c55eb990dbed6829464e9ff014fc5728073
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-07 23:27:12 +0100

    Mixed import from Rx & Promise examples

--- vanilla-js/src/index.js
+++ vanilla-js/src/index.js
@@ -2,26 +2,31 @@ import { WsProvider } from '@polkadot/rpc-provider';
 import './style.css';
 
 import {
-  simpleConnect,
+  // simpleConnect,
   listenToBlocks,
-  listenToBalanceChange,
-  // createAccount,
+  // listenToBalanceChange,
   readChainState,
-  makeTransfer,
-  displaySystemEvents
+  // makeTransfer,
+  displaySystemEvents,
 } from './with-promise-api';
-// import { simpleConnect, listenToBlocks } from './api-calls/with-rx-api';
+import {
+  simpleConnect, // @TODO debug subscriptionId
+  // listenToBlocks,
+  listenToBalanceChange,
+  // readChainState, // @TODO debug 'nonce === null vs. 0' in ApiRx or rpc-rx
+  makeTransfer, // @TODO go through examples in docs
+  // displaySystemEvents,
+} from './with-rx-api';
 
 // Initialise the provider to connect to the local node
 const provider = new WsProvider('ws://127.0.0.1:9944');
 // const provider = new WsProvider('wss://substrate-rpc.parity.io/');
 
-(function main () {
-  simpleConnect(provider).catch(console.error);
-  listenToBlocks(provider).catch(console.error);
-  listenToBalanceChange(provider).catch(console.error);
-  // createAccount(provider).catch(console.error),
-  readChainState(provider).catch(console.error);
-  makeTransfer(provider).catch(console.error);
-  displaySystemEvents(provider).catch(console.error);
+(function main() {
+  simpleConnect(provider);
+  listenToBlocks(provider);
+  listenToBalanceChange(provider);
+  readChainState(provider);
+  makeTransfer(provider);
+  displaySystemEvents(provider);
 }());
