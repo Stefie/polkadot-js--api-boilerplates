bfe93bad2271449129c0c4b446042c2bbc2978ce
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-14 18:42:28 +0100

    Setup for 'upgrade with sudo' example

--- vanilla-js/src/with-promise-api/index.js
+++ vanilla-js/src/with-promise-api/index.js
@@ -4,3 +4,5 @@ export { default as listenToBalanceChange } from './03-listen-to-balance-change'
 export { default as readChainState } from './05-read-chain-state';
 export { default as makeTransfer } from './06-make-transfer';
 export { default as displaySystemEvents } from './07-display-system-events';
+export { default as transferEvents } from './08-transfer-events';
+export { default as upgradeWithSudo } from './09-upgrade-with-sudo';
