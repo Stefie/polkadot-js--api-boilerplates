bfa9c7ccd87a5506f29252e0e234cd198d6136a1
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-15 17:07:51 +0100

    Fix broken paths

--- vanilla-js/src/with-rx-api/index.js
+++ vanilla-js/src/with-rx-api/index.js
@@ -3,5 +3,5 @@ export { default as listenToBlocks } from './02-listen-to-blocks';
 export { default as listenToBalanceChange } from './03-listen-to-balance-change';
 export { default as readChainState } from './05-read-chain-state';
 export { default as makeTransfer } from './06-make-transfer';
-export { default as displaySystemEvents } from './07-display-system-events';
-export { default as transferEvents } from './08-transfer-events';
+export { default as displaySystemEvents } from './08-system-events';
+export { default as transferEvents } from './09-transfer-events';
