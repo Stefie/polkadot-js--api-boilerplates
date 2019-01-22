8f5fff39c5228910b72266a2e762b279466043eb
Stefanie Doll <stefie.doll@googlemail.com>
2019-01-22 16:26:47 +0100

    read_storage example with tap()

--- vanilla-js/src/with-rx-api/05-read-storage.js
+++ vanilla-js/src/with-rx-api/05-read-storage.js
@@ -1,4 +1,4 @@
-import { first, switchMap } from 'rxjs/operators';
+import { first, switchMap, tap } from 'rxjs/operators';
 import { combineLatest, of } from 'rxjs';
 import { ApiRx } from '@polkadot/api';
 import {
@@ -11,34 +11,38 @@ export default (provider) => {
 
   new ApiRx(provider).isReady
     .pipe(
+      // Here we ake our basic chain state/storage queries, all in one go
       switchMap((api) => combineLatest(
+        // We're combining the results together with the emitted value 'api',
+        // which we're turning back into an observable using of()
         of(api),
-        api.query.session.validators().pipe(first())
+        api.query.session.validators().pipe(first()),
+        api.query.system.accountNonce(ALICE).pipe(first()),
+        api.query.timestamp.blockPeriod().pipe(first())
       )),
+      tap(([,, accountNonce, blockPeriod]) => {
+        // We're using tap() to print out the results of our account nonce and block period queries
+        createLog(`Account Alice: ${ALICE} <br />AccountNonce: ${accountNonce}`, wrapper);
+        createLog(`blockPeriod ${blockPeriod.toNumber()} seconds`, wrapper);
+      }),
       switchMap(([api, validators]) => {
-        // If the node has active validators, get the balances
+        // In the next step, we're checking if the node has active validators.
+        // If it does, we're making another call to the api to get the balances for all validators
         const balances = (validators && validators.length > 0)
           ? combineLatest(validators.map(authorityId => api.query.balances.freeBalance(authorityId).pipe(first())))
-          : null;
-
+          : of(null);
         // If there are no validators, we're not returning any balances
         return combineLatest(
-          api.query.system.accountNonce(ALICE).pipe(first()),
-          api.query.timestamp.blockPeriod().pipe(first()),
           of(validators),
           balances
         );
       })
     )
     // Then we're subscribing to the emitted results
-    .subscribe(([accountNonce, blockPeriod, validators, validatorBalances]) => {
-      createLog(`Account Alice: ${ALICE} <br />AccountNonce: ${accountNonce}`, wrapper);
-      createLog(`blockPeriod ${blockPeriod.toNumber()} seconds`, wrapper);
-
+    .subscribe(([validators, validatorBalances]) => {
       if (validatorBalances) {
         createLog('Validators:', wrapper, 'highlight');
-        // And lastly we're subscribing to the observable and print out
-        // the authorityIds and balances of all validators
+        // And lastly we print out the authorityIds and balances of all validators
         validators.forEach((authorityId, index) => {
           createLog(`Validator: ${authorityId.toString()} <br />Balance: ${validatorBalances[index].toString()}`, wrapper);
         });
